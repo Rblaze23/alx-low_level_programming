a95273829e2f6ad51b452861942c15b43b552240
Rblaze23 <117004011+Rblaze23@users.noreply.github.com>
2023-03-19 17:54:30 +0100

    Create 4-print_alphabt.c

---
+++ 4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+	char n='a';
+  for(n="a";n<="z";n++){
+  
+    if(n=="e" or n=="q")
+      continue;
+    putchar(n);
+  }
+  putchar("\n");
+	return (0);
+}
