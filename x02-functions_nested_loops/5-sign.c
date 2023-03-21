457391bfea3dbcd0f2fd6cacdfcc76d56e2a85a9
Rblaze23 <117004011+Rblaze23@users.noreply.github.com>
2023-03-21 10:05:33 +0100

    Create 5-sign.c

---
+++ x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int print_sign(int n){
+if (n>0)
+  printf("+");
+  return 1;
+else if(n==0)
+  printf("0");
+  return 0;
+else
+  printf("-");
+  return -1;
+}
+int main(){
+print_sign();
+return 0;}
