d888a5a9786c693f56693674d381fd81e53da688
Rblaze23 <117004011+Rblaze23@users.noreply.github.com>
2023-03-21 11:03:42 +0100

    Update 11-print_to_98.c

--- x02-functions_nested_loops/11-print_to_98.c
+++ x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+void print_to_98(int n){
+  if(n<=98){
+  for(int i=n;i<=98;i++){printf("%d",i);}
+  }
+  else if(n>98){for(int j=n;j>98;j--){printf("%d",j);}
+}
+int main(){
+  print_to_98(0);
+  print_to_98(98);
+  print_to_98(111);
+  print_to_98(81);
+  print_to_98(-10);
+  return 0;}
