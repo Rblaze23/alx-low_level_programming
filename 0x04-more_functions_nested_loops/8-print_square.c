bd8732f55574d07c18b466d1e51a139d5fba38b9
Rblaze23 <117004011+Rblaze23@users.noreply.github.com>
2023-03-23 13:40:55 +0100

    Create 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void print_square(int size){
+if(size<=0){
+putchar('\n');}
+  for(int i=0;i<size;i++){
+  for(int j=0;j<size;j++){
+  putchar('#');
+  }
+        putchar('\n');
+  }
+}
+
+
+int main(void)
+{
+    print_square(2);
+    print_square(10);
+    print_square(0);
+    return (0);
+}
