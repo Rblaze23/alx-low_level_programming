f0f62c5899001fe081d1dced91577754c8eabe93
Rblaze23 <117004011+Rblaze23@users.noreply.github.com>
2023-03-23 05:33:44 +0100

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void more_numbers(void){
+for(int i=0;i<10;i++){
+for(int j=0;j<15;j++){
+_putchar("0"+j);}
+_putchar("\n");
+}
+}
+int main(void)
+{
+    more_numbers();
+    return (0);
+}
