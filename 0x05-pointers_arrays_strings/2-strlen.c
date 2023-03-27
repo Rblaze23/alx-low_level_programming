39c999c1d0ee591d44b164574cf88de2df36c89a
Rblaze23 <117004011+Rblaze23@users.noreply.github.com>
2023-03-27 13:16:30 +0200

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int _strlen(char *s){
+  int len=0;
+while(*s!='\0'){
+s++;
+  len++;
+}return len;
+}
+int main(void)
+{
+    char *str;
+    int len;
+
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
+    return (0);
+}
