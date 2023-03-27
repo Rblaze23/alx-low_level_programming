b729288c62fa1ed5cf86842fe574c12ec1dc993c
Rblaze23 <117004011+Rblaze23@users.noreply.github.com>
2023-03-27 13:20:31 +0200

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include<stdioh>
+void _puts(char *str){
+  printf("%s\n",str);
+}
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
+}
