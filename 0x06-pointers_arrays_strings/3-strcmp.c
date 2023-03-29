b73d510ce692cfa85757a639f5f504e4695c4e90
Rblaze23 <117004011+Rblaze23@users.noreply.github.com>
2023-03-29 18:32:50 +0200

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+int _strcmp(char *s1, char *s2)
+{
+     int i;
+
+    for (i = 0; s1[i] && s2[i]; i++)
+    {
+        if (s1[i] != s2[i])
+        {
+            return (s1[i] - s2[i]);
+        }
+    }
+
+    return (s1[i] - s2[i]);
+}
+int main(void)
+{
+    char s1[] = "Hello";
+    char s2[] = "World!";
+
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
+}
